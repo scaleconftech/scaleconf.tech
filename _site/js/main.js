d0feb37be3b3ddc6ef0c26f74517a463a73ee7d0
Josue Bustamante <josuedanielbust@Josues-MacBook-Air.local>
2017-06-15 23:37:10 -0500

    New header and add icons to important info

--- _site/js/main.js
+++ _site/js/main.js
@@ -1,2 +1,21 @@
-/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-particlesJS.load('particles-js', 'js/particles.json', function() {});
+window.onload = function() {
+Particles.init({
+    selector: '.background',
+    color: '#75A5B7',
+    maxParticles: 80,
+    connectParticles: true,
+    responsive: [
+    {
+        breakpoint: 768,
+        options: {
+        maxParticles: 80
+        }
+    }, {
+        breakpoint: 375,
+        options: {
+        maxParticles: 50
+        }
+    }
+    ]
+});
+};
