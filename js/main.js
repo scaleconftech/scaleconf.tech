df405afd2e5ee5d8da659bc23e7d0e879b112c4e
Josue Daniel Bustamante <josuedanielbustamante@gmail.com>
2017-08-04 16:53:43 -0500

    Change color layout to be more lightfull

--- js/main.js
+++ js/main.js
@@ -1,21 +1,21 @@
 window.onload = function() {
-Particles.init({
-    selector: '.background',
-    color: '#75A5B7',
-    maxParticles: 80,
-    connectParticles: true,
-    responsive: [
-    {
-        breakpoint: 768,
-        options: {
-        maxParticles: 80
+    Particles.init({
+        selector: '.background',
+        color: '#89faef',
+        maxParticles: 80,
+        connectParticles: true,
+        responsive: [
+        {
+            breakpoint: 768,
+            options: {
+            maxParticles: 80
+            }
+        }, {
+            breakpoint: 375,
+            options: {
+            maxParticles: 50
+            }
         }
-    }, {
-        breakpoint: 375,
-        options: {
-        maxParticles: 50
-        }
-    }
-    ]
-});
+        ]
+    });
 };
